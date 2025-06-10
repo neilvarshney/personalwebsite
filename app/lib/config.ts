b7cc626671f9a6350d206a8c1aab514d64421bb4
Neil Varshney <neil4varshney@gmail.com>
2025-06-10 16:20:53 -0400

    Update

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,7 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://neilvarshney.com/",
+  title: "Neil Varshney's Website",
+  name: "Neil Varshney",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
@@ -9,8 +9,8 @@ export const metaData = {
 
 export const socialLinks = {
   twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/neilvarshney/personalwebsite",
+  instagram: "https://www.instagram.com/neil.varshney/",
+  linkedin: "https://www.linkedin.com/in/neil-varshney/",
+  email: "mailto:neilvarshney@cmail.carleton.ca",
 };
