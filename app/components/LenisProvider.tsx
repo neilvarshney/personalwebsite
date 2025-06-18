8a67083a85b7530c0a9e8ab865d5d73d61e563ae
Neil Varshney <neil4varshney@gmail.com>
2025-06-18 00:11:06 -0400

    better mobile appearance

--- app/components/LenisProvider.tsx
+++ app/components/LenisProvider.tsx
@@ -4,6 +4,10 @@ import Lenis from "@studio-freight/lenis";
 
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
+
+    // Force scroll to top on page load
+    window.scrollTo(0, 0);
+
     const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
     const lenis = new Lenis({
       duration: isTouchDevice ? 0.125 : 0.8,
@@ -17,11 +21,6 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
 
     // Expose lenis to window object
     window.lenis = lenis;
-
-    // Ensure we start at the top of the page on reload
-    if (window.location.pathname === '/') {
-      lenis.scrollTo(0, { immediate: true });
-    }
 
     function raf(time: number) {
       lenis.raf(time);
